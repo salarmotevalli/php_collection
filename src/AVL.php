96788e1753ed5a72fdb225f767af3d53a38d2cfe
salarmotevalli <salar.mo.ro@gmail.com>
2023-05-19 19:35:32 +0330

    start project

---
+++ src/AVL.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Salar\Bst;
+
+class AVL
+{
+    private $left_child;
+    private $right_child;
+
+    public function new(): self
+    {
+        return new self();
+    }
+
+    public function from(): self
+    {
+        return new self();
+    }
+}
