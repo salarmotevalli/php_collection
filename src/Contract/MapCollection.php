19987826ef5cb526d8a90012eb8fe0ab36d92472
salarmotevalli <salar.mo.ro@gmail.com>
2023-06-02 21:55:02 +0330

    add docs to interface

--- src/Contract/MapCollection.php
+++ src/Contract/MapCollection.php
@@ -5,57 +5,136 @@ namespace Salar\Contract;
 interface MapCollection
 {
     // Create new
+    /**
+     * @return Collection
+     */
     public static function new(): Collection;
 
     // Create with default value
+
+    /**
+     * @param array $items
+     * @return Collection
+     */
     public static function from(array $items): Collection;
 
     // Insert Node
+
+    /**
+     * @param mixed $key
+     * @param mixed $value
+     * @return void
+     */
     public function insert(mixed $key, mixed $value): void;
 
     // Insert Nodes
+
+    /**
+     * @param array $items
+     * @return void
+     */
     public function multi_insert(array $items): void;
 
     // Clear DataStruct
+
+    /**
+     * @return void
+     */
     public function clear(): void;
 
     // Get first Node
+
+    /**
+     * @return mixed
+     */
     public function first_value(): mixed;    // Get last Node
 
     // Return last value
+    /**
+     * @return mixed
+     */
     public function last_value(): mixed;
 
     // Return first key (smallest)
+
+    /**
+     * @return mixed
+     */
     public function first_key(): mixed;
 
     // Return first key (largest)
+
+    /**
+     * @return mixed
+     */
     public function last_key(): mixed;
 
     // Is collection empty?
+
+    /**
+     * @return bool
+     */
     public function is_empty(): bool;
 
     // Return length of collection
+
+    /**
+     * @return int
+     */
     public function len(): int;
 
     // Remove smallest key
+
+    /**
+     * @return mixed
+     */
     public function pop_first(): mixed;
 
     // Remove largest key
+
+    /**
+     * @return mixed
+     */
     public function pop_last(): mixed;
 
     // Remove given key
-    public function remove(mixed $key);
+
+    /**
+     * @param mixed $key
+     * @return mixed
+     */
+    public function remove(mixed $key): mixed;
 
     // Return list of values
+
+    /**
+     * @return array
+     */
     public function values(): array;
 
     // Return list of keys
+
+    /**
+     * @return array
+     */
     public function keys(): array;
 
     // Return key value pairs
+
+    /**
+     * @return array
+     */
     public function all(): array;
 
+    /**
+     * @param ...$values
+     * @return bool
+     */
     public function in_collection(... $values): bool;
 
+    /**
+     * @param ...$keys
+     * @return bool
+     */
     public function is_set(... $keys): bool;
 }
--- src/Contract/SetCollection.php
+++ src/Contract/SetCollection.php
@@ -5,35 +5,92 @@ namespace Salar\Contract;
 interface SetCollection
 {
     // Create new
+    /**
+     * @return Collection
+     */
     public static function new(): Collection;
 
     // Create with default value
+
+    /**
+     * @param array $values
+     * @return Collection
+     */
     public static function from(array $values): Collection;
 
     // Insert Node
+
+    /**
+     * @param mixed $value
+     * @return void
+     */
     public function insert(mixed $value): void;
 
+    /**
+     * @param array $values
+     * @return void
+     */
     public function multi_insert(array $values): void;
 
     // Clear DataStruct
+
+    /**
+     * @return void
+     */
     public function clear(): void;
 
     // Get first Node
+
+    /**
+     * @return mixed
+     */
     public function first_value(): mixed;
 
     // Is Btree empty?
+
+    /**
+     * @return bool
+     */
     public function is_empty(): bool;
 
         // Get last Node
+
+    /**
+     * @return mixed
+     */
     public function last_value(): mixed;
 
     // return length of DataStruct
+
+    /**
+     * @return int
+     */
     public function len(): int;
 
+    /**
+     * @return mixed
+     */
     public function pop_first(): mixed;
+
+    /**
+     * @return mixed
+     */
     public function pop_last(): mixed;
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
     public function remove(mixed $value);
+
+    /**
+     * @return array
+     */
     public function values(): array;
 
+    /**
+     * @param ...$values
+     * @return bool
+     */
     public function in_collection(... $values): bool;
 }
